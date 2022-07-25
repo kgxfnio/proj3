a93c9b926d80165ffcd9a1cebd84585a62530543
kgxfnio <95566842+kgxfnio@users.noreply.github.com>
2022-07-25 16:07:18 +0300

    Add files via upload

--- projectic/js.js
+++ projectic/js.js
@@ -1,7 +1,12 @@
 let i = 0;
 function AddToList(){
-    let Text = document.createElement('span');
-    Text.innerText = document.getElementById('task').value;
+    if(document.getElementById('first-part').style.height>=document.getElementById('first-part').style.maxHeight){
+        document.getElementById('first-part').style="overflow-y:scroll;"
+    };
+    let Text = document.createElement('input');
+    Text.value = document.getElementById('task').value;
+    Text.disabled = true;
+    Text.className = 'input-block';
     let Container = document.createElement('div');
     Container.id = `container${i}`;
     Container.className = 'elem-container';
@@ -14,9 +19,20 @@ function AddToList(){
     let EditBtn = document.createElement('button');
     EditBtn.className = 'editBtn';
     EditBtn.innerText = 'edit';
+    EditBtn.onclick = function edit(){
+        if(this.parentNode.firstElementChild.disabled == true){
+            this.parentNode.firstElementChild.disabled = false;
+            this.innerText = 'save';
+        }
+        else{
+            this.parentNode.firstElementChild.disabled = true;
+            this.innerText = 'edit';
+        }
+    };
     document.getElementById(`first-part`).append(Container);
     document.getElementById(`container${i}`).append(Text);
     document.getElementById(`container${i}`).append(DelBtn);
     document.getElementById(`container${i}`).append(EditBtn); 
     i+=1;
 }
+
